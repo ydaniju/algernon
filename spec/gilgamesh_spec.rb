4b3e04eee6a4970bd0793ff1acb9b17849217dc9
andela-ydaniju <yusuf.daniju@andela.com>
2016-02-16 23:28:45 +0100

    [Finishes 113875909] Define rackup call and Initialize todolist

--- spec/gilgamesh_spec.rb
+++ spec/gilgamesh_spec.rb
@@ -4,4 +4,8 @@ describe Gilgamesh do
   it "has a version number" do
     expect(Gilgamesh::VERSION).not_to be nil
   end
+
+  it "has a call method with an array" do
+    expect(Gilgamesh::Application.new.call("").class).to eq Array
+  end
 end
