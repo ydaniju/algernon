c7d8394338deec7ed47fba57a5de0d3015d3e30c
andela-ydaniju <yusuf.daniju@andela.com>
2016-03-05 08:20:16 +0100

    [FInishes 115088195] Using Controller name for views

--- lib/algernon/controller/controls.rb
+++ lib/algernon/controller/controls.rb
@@ -9,10 +9,20 @@ module Algernon
       end
 
       def render(view_name, locals = {})
-        filename = File.join("app", "views", "#{view_name}.html.erb")
+        filename = File.join("app",
+                             "views",
+                             controller_for_views,
+                             "#{view_name}.html.erb"
+                            )
         template = File.read(filename)
         eruby = Erubis::Eruby.new(template)
         eruby.result(locals.merge(env: env))
+      end
+
+      def controller_for_views
+        controller_class = self.class
+        controller_class = controller_class.to_s.gsub(/Controller$/, "")
+        controller_class.snakify
       end
     end
   end
