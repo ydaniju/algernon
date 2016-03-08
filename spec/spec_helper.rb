d7ce3d1361e62084a080c429530ea1b1e9c7dd1c
andela-ydaniju <yusuf.daniju@andela.com>
2016-03-08 07:14:20 +0100

    [Finishes 1151111063] Table builder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require "coveralls"
 Coveralls.wear!
 
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "lapis_todo/config/application.rb"
+require "integration/lapis_todo/config/application.rb"
 require "rspec"
 require "algernon"
 require "rack/test"
