e7ea395424ce27da0cbea01951aefbfd35fd83d8
andela-ydaniju <yusuf.daniju@andela.com>
2016-03-10 13:24:04 +0100

    [Finishes 115410673] Complete Integrations

--- spec/integration/task_life_cycle_spec.rb
+++ spec/integration/task_life_cycle_spec.rb
@@ -7,14 +7,26 @@ describe "Task lifecycle", type: :feature do
     fill_in "Description", with: "Go to the deepest Ocean"
     click_button "Create Task"
 
+    visit "/"
+    click_link_or_button "New Task"
+    fill_in "Title", with: "Fly"
+    fill_in "Description", with: "India Airways"
+    click_button "Create Task"
+
     click_link "Edit"
 
-    fill_in "Title", with: "Scuba Diving"
-    fill_in "Description", with: "Go to the deepest Ocean"
+    fill_in "Description", with: "Ethiopian Airways"
     click_link_or_button "Effect Change"
+
+    count = Task.all.length
+
+    expect(count).to eq 2
 
     visit "/tasks/1"
     click_link "Delete"
+
+    Task.destroy_all
+
     expect(Task.all).to eq []
   end
 end
