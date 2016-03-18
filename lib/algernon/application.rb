eafa280c0906626d8e4ba02f5ca3156e9bae72a6
andela-ydaniju <yusuf.daniju@andela.com>
2016-03-18 10:06:47 +0100

    Make changes to file structure and names.
    1. Method override was moved from controller directory to utility
    2. RequestHandler class and file was changed to Dispatcher
    3. The DB constant in the extending Model class for Orm Module was made to be Algernon::Orm
    4. Changed the method columns_array to columns

--- lib/algernon/application.rb
+++ lib/algernon/application.rb
@@ -1,5 +1,5 @@
 require "rack"
-require "algernon/controller/request_handler"
+require "algernon/controller/dispatcher"
 require "algernon/utility/method_override"
 
 module Algernon
@@ -25,7 +25,7 @@ module Algernon
     def respond_to_request
       route = router.get_match(request.request_method, request.path_info)
       if route
-        handler = RequestHandler.new(request, route)
+        handler = Dispatcher.new(request, route)
         handler.response
       else
         page_not_found
